fb6c61140edc3f8e5a0b7e3bee997f8ecaab7057
ahsanmubashir01 <134580109+ahsanmubashir01@users.noreply.github.com>
2023-09-18 21:36:55 +0500

    16

---
+++ lib/deleteit.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class git extends StatefulWidget {
+  const git({super.key});
+
+  @override
+  State<git> createState() => _gitState();
+}
+
+class _gitState extends State<git> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
